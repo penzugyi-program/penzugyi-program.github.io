6cf172f5eabd466e809558ae3b5ebd2c4f1ac58b
penzugyi-program <66088561+penzugyi-program@users.noreply.github.com>
2020-05-29 01:22:22 +0600

    Add files via upload

---
+++ terms.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Felhasználási Feltételek</title>
+</head>
+<body>
+<h3>Felhasználási Feltételek</h3>
+
+Ez a dokumentum tartalmazza az ABCMobile OÜ (a továbbiakban: ABC Mobile) által kínált termékek és / vagy szolgáltatások (a továbbiakban: Szolgáltatások) használatát szabályozó Általános Szerződési Feltételeket. ABCMobile OÜ  14710834; Сím: Észtország, Tallinn, Szentpétervár út 71-318, 11415. A Szolgáltatások használatával és / vagy az ABC Mobile termékek bármelyikének telepítésével a felhasználó megerősíti, hogy betöltötte a 18. életévét, és elfogadja, hogy a jelen Feltételek, valamint Adatvédelmi Irányelvek jogilag kötelezőek. Megjegyzés vagy kérdés esetén kérjük, küldjön e-mailt a support@abcmobile.com címre.<br>
+<br>
+Általános Feltételek<br>
+<br>
+A Szolgáltató kizár minden felelősséget vagy garanciát az információk pontosságára, relevanciájára vagy minőségére vonatkozóan, és csak szórakoztató célokat szolgál. A Szolgáltató nem vállal felelősséget semmilyen személynek vagy szervezetnek, semmilyen veszteségért vagy kárért, (beleértve, de nem kizárólag, fizikai, szellemi, különleges vagy közvetett) amelyet a Szolgáltatáson keresztül nyújtott bármilyen tájékoztatás és/vagy javaslat, tanács okozott, vagy a Szolgáltatás használatának eredménye, függetlenül attól, hogy hanyagságból, balesetből vagy bármilyen más okból származik-e<br>
+<br>
+Az egészségügyi állapottal kapcsolatos tanácsokat engedéllyel rendelkező orvosnak kell ellenőriznie, mielőtt cselekedne.<br>
+<br>
+Emelt díjas szolgáltatás<br>
+<br>
+A szolgáltatások igénybevételéhez vagy a számlafizetőnek kell lennie, vagy a számlafizető engedélyére van szüksége a hívás kezdeményezéséhez. A szolgáltatás igénybevétele Magyarországon belül 508 FT (ÁFÁVAL) / perc áron érhető el, ami a telefonszolgáltatója hozzáférési díját még nem tartalmazza. A maximális hívás hossza 30 perc. A hívások rögzítésre kerülnek, csak szórakoztatási célra, 18 év felett. Az ügyfélszolgálathoz kérjük, hívja a +36 14554695 számot.<br>
+<br>
+Jogi nyilatkozat, szerzői jog és egyéb feltételek<br>
+<br>
+Minden termék és szolgáltatás szerzői jogi védelem alá esik, és nem szabad másolni, megosztani, szerkeszteni, módosítani vagy terjeszteni.<br>
+<br>
+Az ABC Mobile fenntartja magának a jogot, hogy ideiglenesen vagy véglegesen módosítsa vagy megszüntesse a nyújtott szolgáltatásokat, ha:<br>
+<br>
+- Visszaélt a szolgáltatásunkkal valamilyen módon;<br>
+<br>
+- Visszaélt a cég alkalmazottaival vagy az olvasókkal;<br>
+<br>
+- Csalást követett el.<br>
+<br>
+Az ABC Mobile fenntartja magának a jogot, hogy a szolgáltatás javítása érdekében módosítsa a termékeket vagy szolgáltatásokat. Javasoljuk, hogy rendszeresen ellenőrizze a webhelyet a szolgáltatásokkal kapcsolatos frissítések megtekintéséhez.<br>
+<br>
+Ez az oldal olyan linkeket tartalmazhat, amelyek harmadik félhez tartoznak, és amelyek nem kapcsolódnak az ABC Mobile-hoz. Az ezen oldalakon megjelenített tartalom nem az ABC Mobile felelőssége, és nem támogatja ezeket a weboldalakat vagy szolgáltatásokat.<br>
+<br>
+Ön vállalja, hogy teljes mértékben kártalanítja és mentesíti az ABC Mobile-ot, annak tisztségviselőit, igazgatóit, alkalmazottait, ügynökeit és szolgáltatóit minden olyan költséggel, kiadással, veszteséggel, kárral, követeléssel és felelősséggel kapcsolatban, amely az ABC Mobile webhely használatával, vagy a szolgáltatásban való részvétellel kapcsolatban felmerülhet.
+Irányadó Jog. <br>
+<br>
+A jelen Általános Szerződési Feltételeket a magyar jog szabályozza, és Ön, valamint az ABCMobile OÜ beleegyezik abba, hogy a Magyar Bíróság kizárólagos joghatósága alá tartozik.<br>
+
+
+</body>
+</html>
